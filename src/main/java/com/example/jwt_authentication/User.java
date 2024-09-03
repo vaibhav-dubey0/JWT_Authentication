d6eecddbd06461160181318a7377d38293b13ae9
Vaibhav Dubey <dubeyvaibhav482@gmail.com>
2024-09-03 23:00:48 +0530

    First

---
+++ src/main/java/com/example/jwt_authentication/User.java
@@ -0,0 +1,20 @@
+package com.example.jwt_authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+     
+    
+    private int id;
+    private String name;
+    
+}
